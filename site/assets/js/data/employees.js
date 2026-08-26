cb4100353570ec6ab456b309cf197830e81b3b46
AccelAnalysis <jholman@accelanalysis.com>
2026-08-26 16:40:47 -0400

    Add current handlers to org chart employee roster

--- site/assets/js/data/employees.js
+++ site/assets/js/data/employees.js
@@ -1,2 +1,11 @@
 'use strict';
-export const EMPLOYEES=['Vacant / TBD','Ronald Brawner','Tim Baird','Chris Casora','Kyle Davis','Matthew Donegan-Cheney','Ryan Gibbs','Kenneth Gregory','Michelle Hartman','Stephane Hubert','Jasmine Johnson','James Overton','Kayla Petersen','Jerad Poor','Nigel Rhodes','Bashir Roland','Paul Roushia','Kimberly Smith','Lindsay Smith','Peggy Stoltzfus','Brandon Yon','External / Contractor'];
+export const EMPLOYEES=[
+  'Vacant / TBD',
+  'Ronald Brawner','Tim Baird','Chris Casora','Kyle Davis','Matthew Donegan-Cheney','Ryan Gibbs',
+  'Kenneth Gregory','Michelle Hartman','Stephane Hubert','Jasmine Johnson','James Overton','Kayla Petersen',
+  'Jerad Poor','Nigel Rhodes','Bashir Roland','Paul Roushia','Kimberly Smith','Lindsay Smith','Peggy Stoltzfus',
+  'Brandon Yon',
+  'Brandon Franquez','Thomas Talmade','Jeff Dobert','Merrick Overton','Matthew Hammer','Sydney Williams',
+  'Savannah Barnes','John Tucker','Morgan Anderson','Abbie Hendry','Tobias Wilkie','Traci Faller','Javon Gray',
+  'External / Contractor'
+];
